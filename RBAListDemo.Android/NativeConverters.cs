09bb2ec9fe4e865ff4ec8bf77a7f02c76545225b
mattkrebs <matt@44lights.net>
2012-12-20 13:43:33 -0600

    fixed native converters

--- RBAListDemo.Android/NativeConverters.cs
+++ RBAListDemo.Android/NativeConverters.cs
@@ -9,10 +9,36 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Graphics.Drawables;
+using System.IO;
+using System.Globalization;
+using Android.Graphics;
 
 namespace RBAListDemo.Android
 {
-    class NativeConverters
+   public class NativeConverters
     {
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var bitmapDrawable = (BitmapDrawable)value;
+            using (var ms = new MemoryStream())
+            {
+                bitmapDrawable.Bitmap.Compress(Bitmap.CompressFormat.Jpeg, 70, ms);
+
+                return System.Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var base64 = (string)value;
+
+            var bytes = System.Convert.FromBase64String(base64);
+
+            var drawable = BitmapFactory.DecodeByteArray(bytes, 0, bytes.Length);
+
+            return new BitmapDrawable(drawable);
+        }
     }
 }
